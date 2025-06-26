9f6ab86fb3a96b30c987d0008aa1ce53637268a0
Danica Benjamin <167496252+DaniKitty@users.noreply.github.com>
2025-06-26 12:18:47 +0530

    Create LongestBinarySubsequence.java

---
+++ Medium/LongestBinarySubsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestSubsequence(String s, int k) {
+        int n = s.length();
+        int count = 0;
+        int zero = 0;
+        long pow = 1; 
+        long value = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(s.charAt(i) == '1') {
+                if(pow > k) {
+                    break;
+                }
+                if(value + pow <= k) {
+                    value += pow;
+                    ++count;
+                }
+            }
+            pow *= 2;
+            if(pow > k) {
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '0') {
+                zero++;
+            }
+        }
+        return(count + zero);
+    }
+}
